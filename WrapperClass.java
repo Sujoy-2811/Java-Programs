6f3aea20405625614469e3b0d1ec2bf430d7ce5e
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-07 19:54:54 +0530

    wrapper class

---
+++ WrapperClass.java
@@ -0,0 +1,14 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Integer x = 500, y = 500;
+        System.out.println("value of x : " + x);
+        System.out.println("value of y : " + y);
+
+        if (x == y) {
+            System.out.println("Both are Same");
+        } else {
+            System.out.println("Both are not Same");
+
+        }
+    }
+}
