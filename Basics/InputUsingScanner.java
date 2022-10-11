ecd2df8264e0520181a3056f7543c6523db275c4
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-11 21:54:34 +0530

    input using scanner class

---
+++ Basics/InputUsingScanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputUsingScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  an integer");
+        int x = sc.nextInt();
+        System.out.println("You entered : " + x);
+    }
+}
