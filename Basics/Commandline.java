3eb2187ba3594607db125b161739ad428184fada
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-12 22:05:46 +0530

    Command line arguments

---
+++ Basics/Commandline.java
@@ -0,0 +1,12 @@
+public class Commandline {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            System.out.println("Aruguments are : ");
+            for (String x : args) {
+                System.out.println(x);
+            }
+        } else {
+            System.out.println("No arguments");
+        }
+    }
+}
