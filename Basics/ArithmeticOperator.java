e62174ab34f281e3241a8cfbd047524e0b37c9c4
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-13 08:32:42 +0530

    Arithmetic Operators

---
+++ Basics/ArithmeticOperator.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        System.out.println(x + y);
+        System.out.println(x * y);
+        System.out.println(y / x);
+        System.out.println(y % x);
+        System.out.println(x + 10 * y);
+        int z = x++;
+        System.out.println(z + " " + x);
+        z = ++x;
+        System.out.println(z + " " + x);
+    }
+}
