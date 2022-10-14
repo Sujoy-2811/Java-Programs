ccf3d075372c0074b0e15d3c5867a9b1705ac413
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-14 08:36:04 +0530

    Assignment Operator

---
+++ AssignmentOperator.java
@@ -0,0 +1,11 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 10, y = 5, z;
+        x += y; // x =x+y
+        System.out.println(x);
+        x %= y; // x=x%y
+        System.out.println(x);
+        z = x = y;
+        System.out.println(z);
+    }
+}
