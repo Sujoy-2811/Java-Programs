de1bca3b413d879c43d4a94cbc0d78e292dee62f
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-15 08:54:17 +0530

    Logical Operator

---
+++ Basics/LogicalOperator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * LogicalOperator
+ */
+public class LogicalOperator {
+
+    public static void main(String[] args) {
+        String usr = "sujoy", pwd = "123";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username :");
+        String username = sc.next();
+        System.out.println("Enter password :");
+        String password = sc.next();
+        if (usr.equals(username) && pwd.equals(password)) {
+            System.out.println("Welcome, Sujoy");
+        } else {
+            System.out.println("try again");
+        }
+    }
+}
