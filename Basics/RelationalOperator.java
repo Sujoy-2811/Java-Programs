6ea288983bdd53f69e99bf8f2c1e0bb681f6237b
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-16 09:01:10 +0530

    Relational Operator

---
+++ Basics/RelationalOperator.java
@@ -0,0 +1,11 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+        System.out.println(x == y);
+        System.out.println(x != y);
+        System.out.println(y < x);
+        System.out.println(y > x);
+        System.out.println(x <= y);
+        System.out.println(x >= y);
+    }
+}
