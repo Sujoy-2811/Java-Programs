325096498db985503b80c959e321be2453172525
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-18 16:35:57 +0530

    minor update

--- Basics/BitwiseOperator.java
+++ Basics/BitwiseOperator.java
@@ -4,5 +4,9 @@ public class BitwiseOperator {
         System.out.println(x & y);
         System.out.println(x | y);
         System.out.println(x ^ y);
+        System.out.println(~x);
+        System.out.println(x << 3);
+        System.out.println(x >> 1);
+
     }
 }
