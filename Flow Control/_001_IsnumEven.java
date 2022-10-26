bbaf6c40b61f78a2561e5595b186f2777a34a86f
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-26 18:34:56 +0530

    minor update

--- Flow Control/_001_IsnumEven.java
+++ Flow Control/_001_IsnumEven.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class _001_IsnumEven {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
-        int a = new Scanner(System.in).nextInt();
+        int a = sc.nextInt();
         System.out.println("Entered number is " + evenCheck(a));
+        sc.close();
 
     }
 
