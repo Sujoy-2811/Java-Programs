4aaab3e8696f3561b48bef99491fb163912e7900
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-28 19:15:30 +0530

    while loop

---
+++ Loops/_001_SumUsingWhileLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _001_SumUsingWhileLoop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter nth natural number ");
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n > 0) {
+            ans += n;
+            n--;
+        }
+        System.out.println("Sum of nth natural numbers is " + ans);
+        sc.close();
+    }
+}
