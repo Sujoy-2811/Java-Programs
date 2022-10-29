b8f81f6f1f924d3bdfcd8d8ec5468fe7926f8618
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-29 21:43:32 +0530

    Input using BufferedReader class

---
+++ Basics/Input.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Input {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter  an integer");
+        int x = Integer.parseInt(br.readLine());
+        System.out.println("You entered : " + x);
+    }
+}
