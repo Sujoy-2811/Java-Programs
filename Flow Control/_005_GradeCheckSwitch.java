6abc6db8984724123823e6bc6210734253141806
Sujoy Manna <sujoymanna007@gmail.com>
2022-10-29 19:28:28 +0530

    minor update

--- Flow Control/_005_GradeCheckSwitch.java
+++ Flow Control/_005_GradeCheckSwitch.java
@@ -1,11 +1,14 @@
+import java.lang.management.GarbageCollectorMXBean;
 import java.util.Scanner;
 
 public class _005_GradeCheckSwitch {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your grade (A-F) :");
         char grade = sc.next().charAt(0);
-
+        sc.close();
+        grade = Character.toUpperCase(grade);
         switch (grade) {
             case 'A':
                 System.out.println("Excellent!");
