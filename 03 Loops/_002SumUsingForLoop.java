5408ab2d7444f8db07a0606a7929ee1cb009cd0d
Sujoy Manna <sujoymanna007@gmail.com>
2022-11-02 22:15:06 +0530

    example of for loop

--- 03 Loops/_002SumUsingForLoop.java
+++ 03 Loops/_002SumUsingForLoop.java
@@ -6,8 +6,8 @@ public class _002SumUsingForLoop {
         System.out.println("Enter nth natural number ");
         int n = sc.nextInt();
         int ans = 0;
-        for (; n > 0; n--) {
-            ans += n;
+        for (int i = n; i > 0; i--) {
+            ans += i;
         }
         System.out.println("Sum of nth natural numbers is " + ans);
         sc.close();
