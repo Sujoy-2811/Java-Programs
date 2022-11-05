21e8a9f853bccddb271a716f96981de38bfc0d48
Sujoy Manna <sujoymanna007@gmail.com>
2022-11-05 11:57:37 +0530

    Count digits of number

---
+++ 03 Loops/_006_CountDigits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class _006_CountDigits {
+    public static void main(String[] args) {
+        int num, count = 0, temp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        num = sc.nextInt();
+        temp = num;
+        while (temp > 0) {
+            temp /= 10;
+            count++;
+        }
+        System.out.println(num + " has " + count + " digits.");
+    }
+}
