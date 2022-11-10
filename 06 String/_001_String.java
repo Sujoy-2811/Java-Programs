cb5762a3ae098b538c372b048a7a003d113318ca
Sujoy Manna <sujoymanna007@gmail.com>
2022-11-10 22:21:30 +0530

    String class in java

---
+++ 06 String/_001_String.java
@@ -0,0 +1,13 @@
+/**
+ * _001_String
+ */
+public class _001_String {
+
+    public static void main(String[] args) {
+        String str1 = "Hello Friends";
+        String str2 = new String("How are you?");
+        System.out.println(str1);
+        System.out.println(str2);
+
+    }
+}
